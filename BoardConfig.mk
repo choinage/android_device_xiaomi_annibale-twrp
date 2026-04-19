065d438455242fe052186e46766a5be997263248
Byben <heybyben@gmail.com>
2026-04-19 12:51:23 +0700

    annibale: Disable haptic for now
    Change-Id: I234252411177f8556a7e5b2910ef11ec7ffda1c1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,7 +163,7 @@ TW_EXCLUDE_APEX := true
 TW_HAS_EDL_MODE := true
 
 # Haptic
-FIXED_HAPTICS := true
+FIXED_HAPTICS := false
 
 ifeq ($(FIXED_HAPTICS),true)
    TW_SUPPORT_INPUT_AIDL_HAPTICS := true
