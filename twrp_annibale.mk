284524cb40f98cb3844a5c2fd5590b61d7747c18
choinage <88093924+choinage@users.noreply.github.com>
2026-04-25 16:42:05 +0800

    Update twrp_annibale.mk

--- twrp_annibale.mk
+++ twrp_annibale.mk
@@ -16,7 +16,7 @@ PRODUCT_RELEASE_NAME := annibale
 PRODUCT_DEVICE := annibale
 PRODUCT_NAME := twrp_annibale
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := RedmiTurbo3
+PRODUCT_MODEL := REDMI K90
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Assert
